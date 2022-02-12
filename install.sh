7a25caf1d3699835cfd387189b199567fb311ef2
Pauline P. Narvas (Paw) <pauline@paulinenarvas.com>
2022-02-12 20:30:27 +0000

    remove all but zsh

--- install.sh
+++ install.sh
@@ -1,8 +1,2 @@
 # ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# auto-suggestion plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -Y
